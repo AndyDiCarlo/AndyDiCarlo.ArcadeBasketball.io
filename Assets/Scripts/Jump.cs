9920344fb1b14538b6b1dcf40a40c274b9f76615
maxhrioux <riouxm@wit.edu>
2021-03-30 20:46:44 -0400

    Jump Function
    Created a jump function that works by pressing the space bar.

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+
+    
+	private Vector2 jump;
+    //change this to edit the speed of the jump
+    public float jumpForce = 3.0f;
+    public bool grounded;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //change these values to edit the jump height
+        jump = new Vector2(0.0f, 2.0f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && grounded){
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+    void FixedUpdate(){
+        grounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+}
